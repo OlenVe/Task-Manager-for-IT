5930a2899e713ab70dffd61bf01dfc37f7c20b36
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-04-11 20:52:09 +0200

    add link in task_manager_for_it/settings, add tasker/urls with index, add templates dir with tasker/index

---
+++ tasker/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from tasker.views import index
+
+
+
+app_name = "tasker"
+urlpatterns = [
+    path("", index, name="index"),
+    ]
--- tasker/views.py
+++ tasker/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from tasker.models import Task, Worker, Project
+
+
+@login_required
+def index(request):
+    """View function for home page of site."""
+    
+    num_tasks = Task.objects.all().count()
+    num_workers = Worker.objects.all().count()
+    num_projects = Project.objects.all().count()
+    
+    context = {
+        "num_tasks": num_tasks,
+        "num_workers": num_workers,
+        "num_projects": num_projects,
+    }
+
+    return render(request, "tasker/index.html", context)
