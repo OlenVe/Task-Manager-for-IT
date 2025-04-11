59e072a956663ed578f2980df55a2089a685bd66
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-04-11 23:10:12 +0200

    created forms and views packedges, created dir in templates

--- tasker/views.py
+++ tasker/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
-from tasker.models import Task, Worker, Project
+from tasker.models import Task, Worker, Project, Position
 
 
 @login_required
@@ -19,3 +21,27 @@ def index(request):
     }
 
     return render(request, "tasker/index.html", context)
+
+
+class PositionListView(LoginRequiredMixin, generic.ListView):
+    model = Position
+    queryset = Position.objects.all()
+    template_name = "tasker/position/position_list.html"
+    context_object_name = "positions_list"
+    paginate_by = 10
+
+    def get_context_data(self, object_list=None, **kwargs):
+        context = super(PositionListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["name"] = PositionSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+
+    def get_queryset(self):
+        name = self.request.GET.get("name")
+        if name:
+            return Position.objects.filter(name__icontains=name)
+        return self.queryset
+
