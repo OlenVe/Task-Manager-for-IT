b9d7f154c10e50ee3c43d49b7657f34addfc29cb
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-04-24 00:40:59 +0200

    fix static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,10 +86,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATIC_ROOT = "staticfiles/"
+STATIC_URL = "static/"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
