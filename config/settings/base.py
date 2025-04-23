96c2a2369f0f52e77dde4eab492fc00b88d4ef8e
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-04-23 22:26:54 +0200

    fix STATICFILES_STORAGE

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,7 +85,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
